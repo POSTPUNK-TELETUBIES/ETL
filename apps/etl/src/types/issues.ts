19dc4f5389b0185e40bad848882829eaeaa9eb16
luiggy <luiggylml@gmail.com>
2023-12-04 18:09:57 -0500

    core added model data issues

---
+++ apps/etl/src/types/issues.ts
@@ -0,0 +1,13 @@
+export enum TypeIssue {
+    Bug = 'BUG',
+    CodeSmell = 'CODE_SMELL',
+    SecurityHotspot = 'SECURITY_HOTSPOT',
+    Vulnerability = 'VULNERABILITY',
+}
+
+export enum Severity {
+    Blocker = 'BLOCKER',
+    Critical = 'CRITICAL',
+    Major = 'MAJOR',
+    Minor = 'MINOR',
+}
